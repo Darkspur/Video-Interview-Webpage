690dab8928c92c4493a66d28d806c723e2d3d5d1
Darkspur <sawant.sahil194@gmail.com>
2020-08-19 13:01:57 +0530

    first commit

---
+++ function.js
@@ -0,0 +1,128 @@
+var interview = [{"question": "Question 1","rating": 0, "comment": "No Comment"},
+                {"question": "Question 2","rating": 0, "comment": "No Comment"},
+                {"question": "Question 3","rating": 0, "comment": "No Comment"},
+                {"question": "Question 4","rating": 0, "comment": "No Comment"},
+                {"question": "Question 5","rating": 0, "comment": "No Comment"}
+                ];
+console.log(interview.length);
+let q = 0;
+document.getElementById("question").innerHTML = interview[q].question;
+document.getElementById("prev").style.visibility = "hidden";
+
+showNextQuestion = () => {
+    if(q < interview.length-2)
+    {
+        q++;
+        document.getElementById("question").innerHTML = interview[q].question;
+        document.getElementById("prev").style.visibility = "visible";
+        console.log(interview);
+    }
+    else if(q == interview.length-2)
+    {
+        q++;
+        document.getElementById("question").innerHTML = interview[q].question;
+        document.getElementById("save").innerHTML = "Save & Finish";
+        document.getElementById("next").style.visibility = "hidden";
+    }
+    else if(q == interview.length-1)
+    {
+        alert("This is the alst question");
+    }
+}
+
+showPrevQuestion = () => {
+    if(q == interview.length-1)
+    {
+        document.getElementById("next").style.visibility = "visible";
+        document.getElementById("save").innerHTML = "Save";
+        document.getElementById("next").innerHTML = ">";
+        q--;
+        document.getElementById("question").innerHTML = interview[q].question;
+    }
+    else if(q > 0)
+    {
+        q--;
+        document.getElementById("question").innerHTML = interview[q].question;
+        if(q == 0)
+            document.getElementById("prev").style.visibility = "hidden";
+    }
+    else if(q == 0)
+    {
+        alert("Can't go back further");
+    }
+    
+}
+
+var rating = 0;
+addRating1 = () => {
+    rating = document.getElementById("ratingBtn1").value;
+    console.log(interview[q].rating);
+}
+addRating2 = () => {
+    rating = document.getElementById("ratingBtn2").value;
+    console.log(interview[q].rating);
+}
+addRating3 = () => {
+    rating = document.getElementById("ratingBtn3").value;
+    console.log(interview[q].rating);
+}
+addRating4 = () => {
+    rating = document.getElementById("ratingBtn4").value;
+    console.log(interview[q].rating);
+}
+addRating5 = () => {
+    rating = document.getElementById("ratingBtn5").value;
+    console.log(interview[q].rating);
+}
+saveResponse = () =>{
+    if(q < interview.length-2)
+    {
+        interview[q].comment = document.getElementById("comment").value;
+        document.getElementById("prev").style.visibility = "visible";
+        document.getElementById("comment").value = "";
+        interview[q].rating = rating;
+        rating = 0;
+        q++;
+        document.getElementById("question").innerHTML = interview[q].question;
+        console.log(interview);
+    }
+    else if(q == interview.length-2)
+    {
+        interview[q].comment = document.getElementById("comment").value;
+        document.getElementById("comment").value = "";
+        interview[q].rating = rating;
+        document.getElementById("next").style.visibility = "hidden";
+        rating = 0;
+        q++;
+        document.getElementById("question").innerHTML = interview[q].question;
+        document.getElementById("save").innerHTML = "Save & Finish";
+        console.log(interview);
+    }
+    else if(q == interview.length-1)
+    {
+        var flag = 0;
+        interview[q].comment = document.getElementById("comment").value;
+        interview[q].rating = rating;
+        rating = 0;
+        var missed = [];
+        for(var i = 0; i < interview.length; i++)
+        {
+            if(interview[i].rating == 0)
+            {
+                missed.push(i+1);
+                flag = -1;
+            }
+        }
+        if(flag == -1)
+        {
+            console.log(missed);
+            alert("You have missed questions: " + missed);
+        }
+        else if(flag == 0)
+        {
+            var interviewJSON = JSON.stringify(interview);
+            localStorage.setItem('questions',interviewJSON);
+            location.href = "InterviewResults.html";
+        }
+    }	
+}
